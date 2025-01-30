de40579c23d3385e771c127c4e9ddb95909ecc44
Harish <hareeshseen@gmail.com>
2025-01-30 10:32:20 +0530

    lifecycle hooks

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -12,18 +12,50 @@ export class ChildComponent {
 
   constructor() {
     this.ageParent = 0;
-    console.log("Constructor");
+    console.log('Constructor');
   }
 
   ngOnChanges() {
+    // @Input-bound inputs
     console.log('ngOnChanges');
   }
 
   ngOnInit() {
+    // triggered upon the completion of initializing data-bound or input properties of a component.
     console.log('ngOnInit');
   }
 
   ngDoCheck() {
+    // ngDoCheck() is called immediately after ngOnChanges().
     console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    // Following the first ngDoCheck, the ngAfterContentInit() hook is executed
+    // triggered when the component or directive content has been initialized.
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    // ngAfterContentChecked is called following ngAfterContentInit() and each subsequent ngDoCheck()
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    // The ngAfterViewInit hook is executed after ngAfterContentChecked().
+    // The view is always loaded immediately after the content.
+    // So, it gets triggered after Angular initializes the views and child views of the component
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    // The ngAfterViewChecked hook is called after the ngAfterViewInit() and every subsequent ngAfterContentChecked().
+    // It is triggered after Angular has examined the component’s and child’s views or the view that holds the directive
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    // destroy the component
+    console.log("ngOnDestroy");
   }
 }
